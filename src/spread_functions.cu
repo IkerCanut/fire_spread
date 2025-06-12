c9edd5e53425c34599d9b7913c54a6906049eb97
francoderico <francoderico2001@gmail.com>
2025-06-12 18:41:50 -0300

    Draft medio boludo

--- src/spread_functions.cu
+++ src/spread_functions.cu
@@ -47,50 +47,57 @@ Fire simulate_fire(
     const Landscape& landscape, const std::vector<std::pair<size_t, size_t>>& ignition_cells,
     SimulationParams params, float distance, float elevation_mean, float elevation_sd, int &contador,
     float upper_limit = 1.0
-) {
-
-  // Crear un generador de números aleatorios con mt19937
-  std::random_device rd;
-  std::mt19937 rng(rd());
-  std::uniform_real_distribution<float> uniform_dist(0.0, 1.0);
-
-  size_t n_row = landscape.height;
-  size_t n_col = landscape.width;
-
-  std::vector<std::pair<size_t, size_t>> burned_ids;
-
-  size_t start = 0;
-  size_t end = ignition_cells.size();
-
-  for (size_t i = 0; i < end; i++) {
-    burned_ids.push_back(ignition_cells[i]);
+  ) {
+    
+    // Crear un generador de números aleatorios con mt19937
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_real_distribution<float> uniform_dist(0.0, 1.0);
+    
+    size_t n_row = landscape.height;
+    size_t n_col = landscape.width;
+    
+    std::vector<std::pair<size_t, size_t>> burned_ids;
+    
+    size_t start = 0;
+    size_t end = ignition_cells.size();
+    
+    std::pair<size_t, size_t>* array;
+    array = (std::pair<size_t, size_t>*)malloc(sizeof(std::pair<size_t, size_t>) * ignition_cells.size());
+    
+    for (size_t i = 0; i < end; i++) {
+      burned_ids.push_back(ignition_cells[i]);
+    array[i] = ignition_cells[i];
   }
-
+  
   std::vector<size_t> burned_ids_steps;
   burned_ids_steps.push_back(end);
-
+  
   size_t burning_size = end + 1;
-
+  
   Matrix<bool> burned_bin = Matrix<bool>(n_col, n_row);
-
+  
   for (size_t i = 0; i < end; i++) {
     burned_bin[ignition_cells[i]] = 1;
   }
-
+  
   double t = omp_get_wtime();
   while (burning_size > 0) {
     size_t end_forward = end;
     
     // Loop over burning cells in the cycle
     
+    size_t sz = end-start;
+    int cnt_threads = 32;
+    int cnt_blocks = (sz+cnt_threads-1)/cnt_threads; // ceil of sz/cnt_threads
+    evaluate_all<<<cnt_blocks, cnt_threads>>>(array, start, end);
+    
     // b is going to keep the position in burned_ids that have to be evaluated
     // in this burn cycle
     for (size_t b = start; b < end; b++) {
       size_t burning_cell_0 = burned_ids[b].first;
       size_t burning_cell_1 = burned_ids[b].second;
-
       const Cell& burning_cell = landscape[{ burning_cell_0, burning_cell_1 }];
-
       constexpr int moves[8][2] = { { -1, -1 }, { -1,  0 }, { -1, 1 }, { 0, -1 },
                                     {  0,  1 }, {  1, -1 }, {  1, 0 }, { 1,  1 } };
 
